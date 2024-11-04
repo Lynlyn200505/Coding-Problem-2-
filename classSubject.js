2769d4d2d68e598b7b5bf4e38fd5bae7724dd87e
Lynlyn200505 <ailynlucina1727@gmail.com>
2024-11-04 18:22:23 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,8 @@
+let subjectTitle = "Art Appreciation"
+let classSchedule = "8:30-10:00 in Monday and Friday"
+let classroom = "Computer Laboratory B"
+let classInstructor = "Mr. Michael Gem Marquez"
+let studentName = "Ailyn L. Gasatan"
+
+
+console.log(${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.)
